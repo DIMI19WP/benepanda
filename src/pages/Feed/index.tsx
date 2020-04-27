e4c301a37edec3795d97ca3baa0e358efa7eae79
reactericdev <reactdev@kakao.com>
2020-04-28 00:24:02 +0900

    make bottom sheet typescript linting

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,26 +1,34 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
-  Text, TouchableNativeFeedback,
+  Text, TouchableNativeFeedback, SectionList, Dimensions, BackHandler,
 } from 'react-native';
-import Orientation, { addOrientationListener } from 'react-native-orientation';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Orientation from 'react-native-orientation';
+import moment from 'moment';
+import 'moment/locale/ko';
 
 import PageWithTitle from 'templetes/PageWithTitle';
 import { Workpaper } from 'types/workpaper';
 import getWorkpapers from 'functions/benedu/getWorkpapers';
 import Panda from 'assets/panda.svg';
-import moment from 'moment';
-import 'moment/locale/ko';
-
-import { FlatList } from 'react-native-gesture-handler';
 import {
-  EmptyWrapper, InfoKey, PaperWrapper, ExamTitle, InfoWrapper,
+  EmptyWrapper, InfoKey, PaperWrapper, PaperTitle, InfoWrapper,
   LeftDate, NoWorkpaper, PandaWrapper, PaperMainInfo, QuestionQuantityBadge,
+  BottomSheetContentWrapper, BottomSheetWrapper,
 } from './styleds';
+import DownloadModal from './DownloadModal';
 
 export default (): JSX.Element => {
   const [workpapers, setWorkpapers] = useState<Workpaper[] | null>(null);
   const [refreshing, setRefreshing] = useState(false);
-  const [orientation, setOrientation] = useState(Orientation.getInitialOrientation());
+  const [screenHeight, setScreenHeight] = useState(Dimensions.get('window').height);
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const [selectedPaper, selectPaper] = useState<{
+    title: string;
+    paperId: string;
+  }>();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const bottomSheetRef = useRef<any>();
   const sortedWorkpapers = workpapers?.reduce((acc: {
     [key: string]: Workpaper[];
   }, paper) => {
@@ -34,13 +42,24 @@ export default (): JSX.Element => {
     (async (): Promise<void> => {
       setWorkpapers(await getWorkpapers());
     })();
-
-    addOrientationListener(setOrientation);
-
-    return () => {
-      Orientation.removeOrientationListener(setOrientation);
-    };
+    function orientListener(): void {
+      setScreenHeight(Dimensions.get('window').height);
+    }
+    Orientation.addOrientationListener(orientListener);
+    return (): void => Orientation.removeOrientationListener(orientListener);
   }, []);
+  useEffect(() => {
+    function closeModal(): boolean {
+      if (isModalOpened) {
+        bottomSheetRef.current.snapTo(1);
+        bottomSheetRef.current.snapTo(1);
+        return true;
+      }
+      return false;
+    }
+    BackHandler.addEventListener('hardwareBackPress', closeModal);
+    return (): void => BackHandler.removeEventListener('hardwareBackPress', closeModal);
+  }, [isModalOpened]);
   useEffect(() => {
     getWorkpapers().then((updatedWorkpapers) => {
       setWorkpapers(updatedWorkpapers);
@@ -49,58 +68,79 @@ export default (): JSX.Element => {
   }, [refreshing]);
   if (workpapers === null) return (<></>);
   return (
-    <PageWithTitle titleText={`밀린 과제 ${workpapers.length}개`}>
-      {workpapers.length !== 0 ? (
-        sortedWorkpapers && (
-          Object.keys(sortedWorkpapers).map((key) => ({
-            title: key,
-            data: sortedWorkpapers[key],
-          })).map(({ title, data: papers }) => (
-            <Fragment key={title}>
-              <LeftDate>{title}</LeftDate>
-              <FlatList
-                data={papers}
-                style={orientation === 'LANDSCAPE' ? { width: 300 } : {}}
-                keyExtractor={({ examTitle }) => examTitle}
-                renderItem={({ item: paper }) => (
-                  <TouchableNativeFeedback
-                    key={paper.examTitle}
-                  >
-                    <PaperWrapper>
-                      <PaperMainInfo>
-                        <ExamTitle>{paper.examTitle}</ExamTitle>
-                        <InfoWrapper>
-                          <InfoKey>과목</InfoKey>
-                          <Text>{paper.subject}</Text>
-                        </InfoWrapper>
-                        <InfoWrapper>
-                          <InfoKey>출제자</InfoKey>
-                          <Text>{paper.register}</Text>
-                        </InfoWrapper>
-                        <InfoWrapper>
-                          <InfoKey>시작일</InfoKey>
-                          <Text>{(paper.startedAt?.format('M월 D일'))}</Text>
-                        </InfoWrapper>
-                      </PaperMainInfo>
-                      <QuestionQuantityBadge>
-                        {paper.questionQuantity}
-                        문제
-                      </QuestionQuantityBadge>
-                    </PaperWrapper>
-                  </TouchableNativeFeedback>
-                )}
-              />
-            </Fragment>
-          ))
-        )
-      ) : (
-        <EmptyWrapper>
-          <PandaWrapper>
-            <Panda />
-          </PandaWrapper>
-          <NoWorkpaper>과제가 없어요!</NoWorkpaper>
-        </EmptyWrapper>
-      )}
-    </PageWithTitle>
+    <>
+      <PageWithTitle titleText={`밀린 과제 ${workpapers.length}개`}>
+        {workpapers.length !== 0 ? (
+          sortedWorkpapers && (
+          <SectionList
+            sections={Object.keys(sortedWorkpapers).map((key) => ({
+              title: key,
+              data: sortedWorkpapers[key],
+            }))}
+            keyExtractor={(paper): string => paper.paperTitle}
+            // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+            renderSectionHeader={({ section: { title } }) => <LeftDate>{title}</LeftDate>}
+            refreshing={refreshing}
+            onRefresh={(): void => setRefreshing(true)}
+            // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+            renderItem={({ item: paper }) => (
+              <TouchableNativeFeedback
+                key={paper.paperTitle}
+                onPress={(): void => {
+                  selectPaper({
+                    paperId: paper.paperId,
+                    title: paper.paperTitle,
+                  });
+                  bottomSheetRef.current.snapTo(0);
+                  bottomSheetRef.current.snapTo(0);
+                }}
+              >
+                <PaperWrapper>
+                  <PaperMainInfo>
+                    <PaperTitle>{paper.paperTitle}</PaperTitle>
+                    <InfoWrapper>
+                      <InfoKey>과목</InfoKey>
+                      <Text>{paper.subject}</Text>
+                    </InfoWrapper>
+                    <InfoWrapper>
+                      <InfoKey>출제자</InfoKey>
+                      <Text>{paper.register}</Text>
+                    </InfoWrapper>
+                    <InfoWrapper>
+                      <InfoKey>시작일</InfoKey>
+                      <Text>{(paper.startedAt?.format('M월 D일'))}</Text>
+                    </InfoWrapper>
+                  </PaperMainInfo>
+                  <QuestionQuantityBadge>
+                    {paper.questionQuantity}
+                    문제
+                  </QuestionQuantityBadge>
+                </PaperWrapper>
+              </TouchableNativeFeedback>
+            )}
+          />
+          )
+        ) : (
+          <EmptyWrapper>
+            <PandaWrapper>
+              <Panda />
+            </PandaWrapper>
+            <NoWorkpaper>과제가 없어요!</NoWorkpaper>
+          </EmptyWrapper>
+        )}
+      </PageWithTitle>
+      <BottomSheet
+        snapPoints={[screenHeight, 0]}
+        initialSnap={1}
+        ref={bottomSheetRef}
+        onOpenStart={() => setIsModalOpened(() => true)}
+        onCloseEnd={() => setIsModalOpened(() => false)}
+        renderContent={() => (
+          <BottomSheetWrapper>
+            <DownloadModal paper={selectedPaper} />
+          </BottomSheetWrapper>
+        )}
+      />
+    </>
   );
 };
