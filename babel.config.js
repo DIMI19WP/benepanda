8fcbe796a5138a74946aa7835854675250d60958
rycont <reactdev@kakao.com>
2020-05-16 17:35:46 +0900

    it's runaable but not buildable

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,9 @@ module.exports = {
     [
       'module-resolver',
       {
-        root: ['./src'],
-        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '~/*': './ src',
+        },
       },
     ],
   ],
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -3,23 +3,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import {
   createStackNavigator,
 } from '@react-navigation/stack';
-import AsyncStorage from '@react-native-community/async-storage';
 
-import loginWithBenedu from 'functions/benedu/login';
+import loginWithBenedu from '~/functions/benedu/login';
 import Feed from './Feed';
 import SignIn from './SignIn';
 
 const Stack = createStackNavigator();
 
-export default () => {
+export default (): JSX.Element => {
   const [loginState, setLoginState] = useState<boolean | undefined>();
   useEffect(() => {
-    console.log('로그인..?');
     loginWithBenedu({ }).then(() => setLoginState(true)).catch(() => {
       setLoginState(false);
     });
   }, []);
-  console.log(loginState);
   return (
     <NavigationContainer>
       {(loginState !== undefined) && (loginState ? (
